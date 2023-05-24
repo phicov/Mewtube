dc4e3efe0a3ecddcd72549480a7f0bacf0c005e0
Philippe Covington <philippehcbusi@gmail.com>
2023-05-24 15:42:36 -0400

    NavBar

---
+++ youtube-mockup/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import {
+  NavBar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
+
+const App = () => (
+  <Router>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <NavBar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/Channel/:id" element={<ChannelDetail />} />
+        <Route path="/Search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </Router>
+);
+
+export default App;
