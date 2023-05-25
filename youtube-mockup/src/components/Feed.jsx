f1dc5d0242bf17a6425e910499c9181328a01011
Philippe Covington <philippehcbusi@gmail.com>
2023-05-25 13:19:27 -0400

    Dynamic Sidebar Titles/Video API Call

--- youtube-mockup/src/components/Feed.jsx
+++ youtube-mockup/src/components/Feed.jsx
@@ -1,8 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
 
-const Feed = () => (
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Sidebar, Videos } from "./";
+
+const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]) 
+
+  return (
   <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
     <Box
       sx={{
@@ -11,18 +23,30 @@ const Feed = () => (
         px: { sx: 0, md: 2 },
       }}
     >
-      <Sidebar />
+      <Sidebar 
+        selectedCategory = 
+        {selectedCategory}
+        setSelectedCategory = 
+        {setSelectedCategory}
+      />
 
-      <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff"}}>
+      <Typography
+        className="copyright"
+        variant="body2"
+        sx={{ mt: 1.5, color: "#fff" }}
+      >
         Copyright 2023 Mewtube
       </Typography>
     </Box>
-    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
-      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
-        New <span style={{ color: '#F31503'}}>Videos</span>
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        {selectedCategory} <span style={{ color: "#F31503" }}>Videos</span>
       </Typography>
+
+      <Videos videos={[]} />
     </Box>
   </Stack>
-);
+  )
+};
 
 export default Feed;
--- youtube-mockup/src/components/Sidebar.jsx
+++ youtube-mockup/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { categories } from "../utils/constants";
 
 const selectedCategory = "New";
 
-const Sidebar = () => (
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
   <Stack
     direction="row"
     sx={{
@@ -16,6 +16,8 @@ const Sidebar = () => (
     {categories.map((category) => (
       <button
         className="category-btn"
+      
+        onClick={() => setSelectedCategory(category.name)}
         style={{
           background: category.name === selectedCategory && "#FC1503",
           color: "white",
