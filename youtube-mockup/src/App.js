e4e7eb3425dfc25810b6369891cba2b88ee788b5
Philippe Covington <philippehcbusi@gmail.com>
2023-06-01 12:28:27 -0400

    Search Feed/Video Details

--- youtube-mockup/src/App.js
+++ youtube-mockup/src/App.js
@@ -17,7 +17,7 @@ const App = () => (
         <Route path="/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/Channel/:id" element={<ChannelDetail />} />
-        <Route path="/Search/:searchTerm" element={<SearchFeed />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
   </Router>
